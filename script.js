f78abc6ef250cb3fa467de1fe96bc803eea331e9
luancauthu2907 <luancauthu@gmail.com>
2021-08-25 16:07:03 +0700

    update final

--- script.js
+++ script.js
@@ -4,38 +4,18 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
-console.log(form);
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (username.value === "") {
-    showError(username, "Không được để trống");
-  } else {
-    showSuccess(username, "ok chhua");
-  }
-  if (email.value === "") {
-    showError(email, "Không được để trống");
-  } else {
-    if (!isValidEmail(email.value)) {
-      showError(email, "Email không đúng định dạng");
-    } else {
-      showSuccess(email, "ok chhua");
+const checkRequired = (inputArray) => {
+  inputArray.forEach((item) => {
+    if (item.value === "") {
+      showError(
+        item,
+        ` ${item.id.charAt(0).toUpperCase()}${item.id.slice(
+          1
+        )} Không được để trống`
+      );
     }
-  }
-  if (password.value === "") {
-    showError(password, "Không được để trống");
-  } else {
-    showSuccess(password, "ok chhua");
-  }
-  if (password2.value === "") {
-    showError(password2, "Không được để trống");
-  } else {
-    if (password2.value !== password.value) {
-      showError(password2, "không khớp với mật khẩu đã nhập ");
-    } else {
-      showSuccess(password2, "ok chhua");
-    }
-  }
-});
+  });
+};
 const isValidEmail = (email) => {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -54,3 +34,34 @@ const showSuccess = (input, message) => {
 
   console.log(formControl);
 };
+const checkLength = (input, min, max) => {
+  if (input.value.length < min || input.value.length > max) {
+    showError(input, `Phải có độ dài từ ${min}  đến ${max}`);
+  } else {
+    showSuccess(input, "ok roi do ");
+  }
+};
+const checkeEmailer = (input) => {
+  if (!isValidEmail(input.value)) {
+    showError(input, "Email không đúng định dạng");
+  } else {
+    showSuccess(input);
+  }
+};
+const checkConfirmPassword = (input, input2) => {
+  if (input.value !== input2.value) {
+    showError(input2, "password không khớp");
+  } else {
+    showSuccess(input2);
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkRequired([username, email, password2, password]);
+  checkLength(password, 3, 10);
+  checkLength(username, 3, 15);
+  checkeEmailer(email);
+  checkConfirmPassword(password, password2);
+});
